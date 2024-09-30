ec99e0095b0bc50a8a465c698555dcc0e8110fad
ZRTstorm <blu3jean@naver.com>
2024-09-30 21:03:29 +0900

    first commit init

---
+++ climbBack/src/main/java/climbing/climbBack/ClimbBackApplication.java
@@ -0,0 +1,13 @@
+package climbing.climbBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbBackApplication.class, args);
+	}
+
+}
---
+++ climbBack/src/test/java/climbing/climbBack/ClimbBackApplicationTests.java
@@ -0,0 +1,13 @@
+package climbing.climbBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

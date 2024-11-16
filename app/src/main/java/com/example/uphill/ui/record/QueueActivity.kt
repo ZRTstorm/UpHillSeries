dc4af9872a1e9227498fed373c41b8a36b5c3da0
LCW <cksdn43@cau.ac.kr>
2024-11-17 01:18:32 +0900

    [upload] addcompitition

--- app/src/main/java/com/example/uphill/ui/record/QueueActivity.kt
+++ app/src/main/java/com/example/uphill/ui/record/QueueActivity.kt
@@ -6,10 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.uphill.R
 
 class QueueActivity : AppCompatActivity() {

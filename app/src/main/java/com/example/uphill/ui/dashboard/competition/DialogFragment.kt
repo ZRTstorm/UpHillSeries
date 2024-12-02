f8f06d6118aff2cb0725cf4fd721759bb078adb1
Yunsu Kim <kys7031010@cau.ac.kr>
2024-12-02 20:08:25 +0900

    참여 코드 복사 기능 추가

--- app/src/main/java/com/example/uphill/ui/dashboard/competition/DialogFragment.kt
+++ app/src/main/java/com/example/uphill/ui/dashboard/competition/DialogFragment.kt
@@ -1,11 +1,14 @@
 package com.example.uphill.ui.dashboard.competition
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.DialogFragment
 import com.example.uphill.R
 import com.example.uphill.data.model.BattleRoomRegistryReceivedData
@@ -29,6 +32,13 @@ class BattleRoomResultDialog(private val data: BattleRoomRegistryReceivedData) :
 
         battleRoomIdTextView.text = "Battle Room ID: ${data.battleRoomId}"
         participantCodeTextView.text = "Participant Code: ${data.participantCode}"
+        participantCodeTextView.setOnClickListener {
+            val clipboard = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as android.content.ClipboardManager
+            val clip = android.content.ClipData.newPlainText("Copied Text", data.participantCode)
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(requireContext(), "복사되었습니다.", Toast.LENGTH_SHORT).show()
+        }
 
         view.findViewById<View>(R.id.closeButton).setOnClickListener {
             parentFragmentManager.setFragmentResult("inviteCodeDialog", Bundle())
